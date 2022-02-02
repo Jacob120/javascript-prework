1f91477b0eb8c0a2248f057be9fc5b054e0e2615
Jakub Kozik <jakubkozik120@gmail.com>
2022-02-02 19:16:13 +0100

    Add variable playerMove

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let computerMove = "kamień";
+
+printMessage(
+  "Zagrałem" + computerMove + "! Jeśli Twój ruch to papier, wygrywasz!"
+);
+
+let playerMove = "papier";
+
+printMessage("Zagrałem" + playerMove);
