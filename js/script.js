78ed077fb4dc87162a047ac959a184a0e37fa4a5
Jakub Kozik <jakubkozik120@gmail.com>
2022-02-02 19:53:55 +0100

    Add computerMove

--- js/script.js
+++ js/script.js
@@ -1,9 +1,27 @@
-let computerMove = "kamień";
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-printMessage(
-  "Zagrałem" + computerMove + "! Jeśli Twój ruch to papier, wygrywasz!"
-);
+console.log("Wylosowana liczba to: " + randomNumber);
 
-let playerMove = "papier";
+let computerMove = "nieznany ruch";
 
-printMessage("Zagrałem" + playerMove);
+if (randomNumber == 1) {
+  computerMove = "kamień";
+} else if (randomNumber == 2) {
+  computerMove = "papier";
+} else if (randomNumber == 3) {
+  computerMove = "nożyce";
+}
+
+printMessage("Mój ruch to: " + computerMove);
+
+let playerInput = prompt("Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.");
+
+console.log("Gracz wpisał: " + playerInput);
+
+let playerMove = "nieznany ruch";
+
+if (playerInput == "1") {
+  playerMove = "kamień";
+}
+
+printMessage("Twój ruch to: " + playerMove);
